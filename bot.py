ef39747ee2a2c28436186e6f1a25176a8ee4cdc5
jackykortrijk <jiaqi.zhao@ugent.be>
2024-09-23 15:42:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,3 +29,31 @@ else:
     # Set up Session State
     if "messages" not in st.session_state:
         st.session_state.messages = [{"role": "assistant", "content": "Hello👋, I'm the UGent ISyE Chatbot🤖! What can I do for you?"},]
+# Submit handler
+def handle_submit(message):
+    """
+    Submit handler:
+
+    You will modify this method to talk with an LLM and provide
+    context using data from Neo4j.
+    """
+
+    # Handle the response
+    with st.spinner('Thinking...'):
+        # # TODO: Replace this with a call to your LLM
+        from time import sleep
+        sleep(1)
+        write_message('assistant', message)
+
+
+# Display messages in Session State
+for message in st.session_state.messages:
+    write_message(message['role'], message['content'], save=False)
+
+# Handle any user input
+if question := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    write_message('user', question)
+
+    # Generate a response
+    handle_submit(question)
