a66baef7b18b47b97688bb335813e73f8b7c4243
jackykortrijk <jiaqi.zhao@ugent.be>
2024-09-23 15:48:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,14 @@ import streamlit as st
 from utils import write_message
 
 # Page Config
-st.set_page_config("Ebert", page_icon=":movie_camera:")
+st.set_page_config("UGent ISyE Chatbot", page_icon="random")
+
+# Show title and description.
+st.title("🤖 UGent ISyE Chatbot")
+st.write(
+    "This is a chatbot that uses OpenAI's GPT-4.0 model to generate responses. "
+)
+
 
 # Set up Session State
 if "messages" not in st.session_state:
